6d2fead639ac573f1c0fb0a71a8f8e48a07bdf03
betty197 <betelhembogale541@gmail.com>
2026-08-07 07:26:45 -0900

    Frontend

--- cleaning-service-app/cleaning-service-backend/models/Users.js
+++ cleaning-service-app/cleaning-service-backend/models/Users.js
@@ -1,7 +1,7 @@
 const { pool } = require("../config/db");
 
+// Get all users
 const getAllUsers = async () => {
-
     const [rows] = await pool.query(
         "SELECT * FROM users"
     );
@@ -10,6 +10,86 @@ const getAllUsers = async () => {
 };
 
 
+// Get user by ID
+const getUserById = async (id) => {
+    const [rows] = await pool.query(
+        "SELECT * FROM users WHERE user_id = ?",
+        [id]
+    );
+
+    return rows[0];
+};
+
+
+// Create user
+const createUser = async (userData) => {
+
+    const {
+        full_name,
+        email,
+        phone,
+        password,
+        role
+    } = userData;
+
+
+    const [result] = await pool.query(
+        `INSERT INTO users 
+        (full_name, email, phone, password, role)
+        VALUES (?, ?, ?, ?, ?)`,
+        [
+            full_name,
+            email,
+            phone,
+            password,
+            role
+        ]
+    );
+
+    return result.insertId;
+};
+
+
+// Update user
+const updateUser = async (id, userData) => {
+
+    const {
+        full_name,
+        email,
+        phone
+    } = userData;
+
+
+    await pool.query(
+        `UPDATE users 
+        SET full_name=?, email=?, phone=?
+        WHERE user_id=?`,
+        [
+            full_name,
+            email,
+            phone,
+            id
+        ]
+    );
+
+};
+
+
+// Delete user
+const deleteUser = async (id) => {
+
+    await pool.query(
+        "DELETE FROM users WHERE user_id=?",
+        [id]
+    );
+
+};
+
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
 };
